ff95e508a44f0cc8165615da651ff1b13768adf5
pms9158@wku.ac.kr <pms9158@wku.ac.kr>
2024-10-23 17:55:56 +0900

    디자인 작업중

---
+++ app/src/main/java/com/example/crittercare/CageControlActivity.java
@@ -0,0 +1,13 @@
+package com.example.crittercare;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CageControlActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cage_control);
+    }
+}
--- app/src/main/java/com/example/crittercare/MainActivity.java
+++ app/src/main/java/com/example/crittercare/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.crittercare;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -15,10 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // 1초 후에 CageControlActivity로 이동
+        new android.os.Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, CageControlActivity.class);
+            startActivity(intent);
+            finish(); // MainActivity 종료
+        }, 1000); // 1000ms = 1초
     }
 }
